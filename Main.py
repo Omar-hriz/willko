7bfd83f6257245433b37a92354ec57c6680f16da
Omar-Hriz <117439952+Omar-hriz@users.noreply.github.com>
2023-05-02 02:05:48 +0200

    read desired properties from file
    l

--- Main.py
+++ Main.py
@@ -1,11 +1,17 @@
-from icalendar import Calendar, Event
-from datetime import datetime
-from pytz import UTC # timezone
+from icalendar import Calendar
 import GenerateFile
-
 
 def display(cal):
     return cal.to_ical().decode("utf-8").replace('\r\n', '\n').strip()
 
 
-print(display(GenerateFile.generate()))
+GenerateFile.create_file()
+file = open('example.ics', 'rb')
+cal_file = Calendar.from_ical(file.read())
+for component in cal_file.walk():
+    if component.name == "VEVENT":
+        print(component.get("organizer"))
+        print(component.get("summary"))
+        print(component.decoded("dtstart"))
+        print(component.decoded("dtend"))
+g.close()
